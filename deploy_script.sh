06924159af363008690718ebd0d9867770cfecb6
Kumar Harsh <hey@kumarharsh.me>
2025-05-12 18:30:56 +0530

    Add host flag

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,7 @@
+eval $(ssh-agent -s)
 chmod 400 deploy_key
 ssh-add deploy_key
-ssh-keyscan -t rsa $SERVER_IP >> ~/.ssh/known_hosts
-ssh $UPCLOUD_USERNAME@$SERVER_IP /bin/bash << 'EOT'
+ssh -o StrictHostKeyChecking=no $UPCLOUD_USERNAME@$SERVER_IP /bin/bash << 'EOT'
   cd random-facts-generator
   git pull origin main
   yarn
