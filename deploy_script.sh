81c78fcd8f368b03c86a2ee53133b8653e6acaa4
Kumar Harsh <hey@kumarharsh.me>
2025-05-12 17:51:24 +0530

    Update deploy script

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,6 +4,7 @@ ssh-add deploy_key
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keyscan -t rsa $SERVER_IP >> ~/.ssh/known_hosts
+cat ~/.ssh/known_hosts
 ssh $UPCLOUD_USERNAME@$SERVER_IP /bin/bash << 'EOT'
   cd random-facts-generator
   git pull origin main
