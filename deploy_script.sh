95ddaf6913f1b32b53d5571789366992d585f834
Kumar Harsh <hey@kumarharsh.me>
2025-05-13 16:52:46 +0530

    Set up Travis CI pipeline

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,7 +2,6 @@ eval $(ssh-agent -s)
 chmod 400 deploy_key
 ssh-add deploy_key
 ssh -o StrictHostKeyChecking=no $UPCLOUD_USERNAME@$SERVER_IP /bin/bash << 'EOT'
-  cd random-facts-generator
   git pull origin main
   yarn
   yarn restart:prod
